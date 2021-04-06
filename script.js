01b53025b6f327f15038203e261366642cb56ba1
miwathecoder <81726290+miwathecoder@users.noreply.github.com>
2021-04-06 06:30:39 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+"use strict";
+/*
+const contactBtn = document.querySelector(".btn"); */
+const modal = document.querySelector("#contact-modal");
+const blurwall = document.querySelector(".blur-wall");
+
+document.querySelector(".btn").addEventListener("click", function () {
+  modal.style.display = "inline";
+  blurwall.style.display = "inline";
+});
+
+document.querySelector(".closex").addEventListener("click", function () {
+  modal.style.display = "none";
+  blurwall.style.display = "none";
+});
